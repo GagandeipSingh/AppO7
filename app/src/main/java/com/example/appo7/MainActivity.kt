c1d0ade7fceab89f7827dd46ee79f17854371fb2
Gagandeip Singh <gagandeips@gmail.com>
2024-06-06 17:58:22 +0530

    Created  Main and Second Activity

--- app/src/main/java/com/example/appo7/MainActivity.kt
+++ app/src/main/java/com/example/appo7/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.appo7
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name:EditText
+    lateinit var college:EditText
+    lateinit var check:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        name = findViewById(R.id.name)
+        college = findViewById(R.id.college)
+        check = findViewById(R.id.check)
+
+            check.setOnClickListener {
+                if(name.text.trim().isEmpty()){
+                    name.error ="Enter Name.."
+                }
+                else if(college.text.trim().isEmpty()){
+                    college.error="Enter College Name.."
+                }
+                else {
+                    Toast.makeText(this, "Correct..", Toast.LENGTH_SHORT).show()
+                    var intent = Intent(this@MainActivity,secondAct::class.java)
+                    startActivity(intent)
+                }
         }
     }
 }
